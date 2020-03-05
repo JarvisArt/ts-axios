01cb51b2c31de87ba2a15bafba0c62589591d3d5
Jarvis <1120886013@qq.com>
2020-03-05 17:53:03 +0800

    feat: XSRF防御

--- src/code/xhr.ts
+++ src/code/xhr.ts
@@ -2,11 +2,24 @@ import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
 
 import { parseHeaders } from '../helpers/headers'
 import { createError } from '../helpers/error'
+import { isURLSameOrigin } from '../helpers/url'
+import cookie from '../helpers/cookie'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
   return new Promise((resolve, reject) => {
-    const { data = null, url, method = 'get', headers, responseType, timeout, cancelToken, withCredentials } = config
+    const {
+      data = null,
+      url,
+      method = 'get',
+      headers,
+      responseType,
+      timeout,
+      cancelToken,
+      withCredentials,
+      xsrfHeaderName,
+      xsrfCookieName
+    } = config
 
     const request = new XMLHttpRequest()
 
@@ -52,6 +65,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     request.ontimeout = function handleTimeout() {
       reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+    }
+
+    if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
+      const xsrfValue = cookie.read(xsrfCookieName)
+      if (xsrfValue && xsrfHeaderName) {
+        headers[xsrfHeaderName] = xsrfValue
+      }
     }
 
     Object.keys(headers).forEach((name) => {
