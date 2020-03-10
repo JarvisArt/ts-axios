6944dcdf286c2cde6d9223e6b1ad543351a6b8b6
Jarvis <1120886013@qq.com>
2020-03-10 15:15:48 +0800

    feat: HTTP授权

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -30,61 +30,73 @@ import { doesNotReject } from 'assert'
 
 /* 上传和下载的进度监控 */
 
-const instance = axios.create()
+// const instance = axios.create()
 
-function calculatePercentage(loaded: number, total: number) {
-  return Math.floor(loaded * 1.0) / total
-}
+// function calculatePercentage(loaded: number, total: number) {
+//   return Math.floor(loaded * 1.0) / total
+// }
 
-function loadProgressBar() {
-  const setupStartProgress = () => {
-    instance.interceptors.request.use(config => {
-      NProgress.start()
-      return config
-    })
+// function loadProgressBar() {
+//   const setupStartProgress = () => {
+//     instance.interceptors.request.use(config => {
+//       NProgress.start()
+//       return config
+//     })
+//   }
+
+//   const setupUpdateProgress = () => {
+//     const update = (e: ProgressEvent) => {
+//       console.log(e)
+//       NProgress.set(calculatePercentage(e.loaded, e.total))
+//     }
+//     instance.defaults.onDownloadProgress = update
+//     instance.defaults.onUploadProgress = update
+//   }
+
+//   const setupStopProgress = () => {
+//     instance.interceptors.response.use(response => {
+//       NProgress.done()
+//       return response
+//     }, error => {
+//       NProgress.done()
+//       return Promise.reject(error)
+//     })
+//   }
+
+//   setupStartProgress()
+//   setupUpdateProgress()
+//   setupStopProgress()
+// }
+
+// loadProgressBar()
+
+// const downloadE1 = document.getElementById('download')
+
+// downloadE1.addEventListener('click', e => {
+//   instance.get('https://img.mukewang.com/5cc01a7b0001a33718720632.jpg')
+// })
+
+// const downloadE2 = document.getElementById('upload')
+
+// downloadE2.addEventListener('click', e => {
+//   const data = new FormData()
+//   const fileE1 = document.getElementById('file') as HTMLInputElement
+//   if (fileE1.files) {
+//     data.append('file', fileE1.files[0])
+
+//     instance.post('/more/upload', data)
+//   }
+// })
+
+/* HTTP 授权 */
+
+axios.post('/more/post', {
+  a: 1
+}, {
+  auth: {
+    username: 'Yes',
+    password: '123456'
   }
-
-  const setupUpdateProgress = () => {
-    const update = (e: ProgressEvent) => {
-      console.log(e)
-      NProgress.set(calculatePercentage(e.loaded, e.total))
-    }
-    instance.defaults.onDownloadProgress = update
-    instance.defaults.onUploadProgress = update
-  }
-
-  const setupStopProgress = () => {
-    instance.interceptors.response.use(response => {
-      NProgress.done()
-      return response
-    }, error => {
-      NProgress.done()
-      return Promise.reject(error)
-    })
-  }
-
-  setupStartProgress()
-  setupUpdateProgress()
-  setupStopProgress()
-}
-
-loadProgressBar()
-
-const downloadE1 = document.getElementById('download')
-
-downloadE1.addEventListener('click', e => {
-  instance.get('https://img.mukewang.com/5cc01a7b0001a33718720632.jpg')
+}).then(res => {
+  console.log(res)
 })
-
-const downloadE2 = document.getElementById('upload')
-
-downloadE2.addEventListener('click', e => {
-  const data = new FormData()
-  const fileE1 = document.getElementById('file') as HTMLInputElement
-  if (fileE1.files) {
-    data.append('file', fileE1.files[0])
-
-    instance.post('/more/upload', data)
-  }
-})
-
